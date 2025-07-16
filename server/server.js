b55144db5a18324af7f8bdc3f88592d5d261ab8b
Priyanka <tayi.priyanka@gmail.com>
2025-07-16 10:29:00 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,31 @@
+import express from "express";
+
+import cors from "cors";
+import { openai } from "./configs/openai.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello from chatGPT",
+  });
+});
+
+app.post("/api/chat", async (req, res) => {
+  const { prompt } = req.body;
+  console.log(req.body);
+  try {
+    const response = await openai.chat.completions.create({
+      model: "gpt-4.1",
+      messages: prompt,
+    });
+    res.status(200).json({ message: response.choices[0].message.content });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+app.listen(5000, () => console.log("server listening on 5000"));
